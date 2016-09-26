53e0e42d3ce1dac894d2506b59236522b6bc7a42
donisetti <donisetti@msn.com>
2016-09-26 17:00:52 -0300

    Controler de Cursos e Professores

---
+++ Universidade.Contoso.Web/ViewModels/AssignedCourseData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Universidade.Contoso.Web.ViewModels
+{
+    public class AssignedCourseData
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
