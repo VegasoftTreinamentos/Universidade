796cffaf778a153f9c7acf2b9048d099aee62252
donisetti <donisetti@msn.com>
2016-09-26 13:54:52 -0300

    Cadastro de Alunos

--- Universidade.Contoso.Web/Controllers/AlunoController.cs
+++ Universidade.Contoso.Web/Controllers/AlunoController.cs
@@ -3,15 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Universidade.Contoso.Dominio.DAL;
+using PagedList;
 
 namespace Universidade.Contoso.Web.Controllers
 {
     public class AlunoController : Controller
     {
-        // GET: Aluno
-        public ActionResult Index()
+        private IStudentRepository studentRepository;
+
+        public AlunoController()
         {
-            return View();
+            this.studentRepository = new StudentRepository(new SchoolContext());
+        }
+
+
+        public AlunoController(IStudentRepository studentRepository)
+        {
+            this.studentRepository = studentRepository;
+        }
+
+
+
+
+        //// GET: Aluno
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        // GET: Aluno
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+
+            var students = from s in studentRepository.GetStudents()
+                           select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.ToUpper().Contains(searchString.ToUpper())
+                                       || s.FirstMidName.ToUpper().Contains(searchString.ToUpper()));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:  // Name ascending 
+                    students = students.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+
+            return View(students.ToPagedList(pageNumber, pageSize));
         }
     }
 }
