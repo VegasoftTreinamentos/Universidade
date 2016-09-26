d249135c402b0a247e2908e427a4eca3e3e10863
donisetti <donisetti@msn.com>
2016-09-26 16:47:15 -0300

    Crud Alunos

--- Universidade.Contoso.Web/Controllers/AlunoController.cs
+++ Universidade.Contoso.Web/Controllers/AlunoController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Universidade.Contoso.Dominio.DAL;
 using PagedList;
+using Universidade.Contoso.Dominio.Entidades;
 
 namespace Universidade.Contoso.Web.Controllers
 {
@@ -78,5 +80,117 @@ namespace Universidade.Contoso.Web.Controllers
 
             return View(students.ToPagedList(pageNumber, pageSize));
         }
+
+        public ViewResult Details(int id)
+        {
+            Student student = studentRepository.GetStudentByID(id);
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Student/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "LastName, FirstMidName, EnrollmentDate")]Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    studentRepository.InsertStudent(student);
+                    studentRepository.Save();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException /* dex */)
+            {
+                //Log the error (uncomment dex variable name after DataException and add a line here to write a log.
+                ModelState.AddModelError(string.Empty, "Unable to save changes. Try again, and if the problem persists contact your system administrator.");
+            }
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            Student student = studentRepository.GetStudentByID(id);
+            return View(student);
+        }
+
+        //
+        // POST: /Student/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PersonID, LastName, FirstMidName, EnrollmentDate")]Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    studentRepository.UpdateStudent(student);
+                    studentRepository.Save();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException /* dex */)
+            {
+                //Log the error (uncomment dex variable name after DataException and add a line here to write a log.
+                ModelState.AddModelError(string.Empty, "Unable to save changes. Try again, and if the problem persists contact your system administrator.");
+            }
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Delete/5
+
+        public ActionResult Delete(bool? saveChangesError = false, int id = 0)
+        {
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem persists see your system administrator.";
+            }
+            Student student = studentRepository.GetStudentByID(id);
+            return View(student);
+        }
+
+        //
+        // POST: /Student/Delete/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                Student student = studentRepository.GetStudentByID(id);
+                studentRepository.DeleteStudent(id);
+                studentRepository.Save();
+            }
+            catch (DataException /* dex */)
+            {
+                //Log the error (uncomment dex variable name after DataException and add a line here to write a log.
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            studentRepository.Dispose();
+            base.Dispose(disposing);
+        }
+
     }
 }
